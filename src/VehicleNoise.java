a522e45ce621b0bc6ca74ddbc3cfbd75b4086b32
kjkleindorfer <kjkleindorfer@dmacc.edu>
2020-07-13 15:40:12 -0500

    Initial Commit

---
+++ src/VehicleNoise.java
@@ -0,0 +1,14 @@
+import model.*;
+
+public class VehicleNoise {
+	//add your new vehicle class to the model package
+	//only push this file and the vehicle class you created
+
+	public static void main(String[] args) {
+		Car honda = new Car();
+		System.out.println(honda.makeNoise());
+		
+
+	}
+
+}
