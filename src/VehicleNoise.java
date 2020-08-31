df536f57c18e0530cc4212938c8901388ad8c19f
Bob Fox <robertfox8@msn.com>
2020-08-31 14:05:15 -0500

    Added Jet
    Added Jet class with "Swoosh!" as the noise.

--- src/VehicleNoise.java
+++ src/VehicleNoise.java
@@ -8,6 +8,9 @@ public class VehicleNoise {
 		Car honda = new Car();
 		System.out.println(honda.makeNoise());
 		
+		Jet gulfStream = new Jet("Gulf Stream V", 14, 530);
+		System.out.println(gulfStream.makeNoise());
+		
 
 	}
 
